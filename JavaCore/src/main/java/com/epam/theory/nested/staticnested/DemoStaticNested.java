625fa6033d754263c4a1d9d88df6dc48e87fa901
Andrei Savich2 <Andrei_Savich2@epam.com>
2022-02-10 13:00:20 +0300

    Added comment

--- JavaCore/src/main/java/com/epam/theory/nested/staticnested/DemoStaticNested.java
+++ JavaCore/src/main/java/com/epam/theory/nested/staticnested/DemoStaticNested.java
@@ -4,6 +4,8 @@ public class DemoStaticNested {
     public static void main(String[] args) {
         Developer devAndrew = new Developer();
         devAndrew.setName("Andrew");
+
+        //Access to static nested classes:
         devAndrew.setLevel(Developer.Level.JUNIOR);
         devAndrew.setTeam(new Developer.Team("Hades"));
 
