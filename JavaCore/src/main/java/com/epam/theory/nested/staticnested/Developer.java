bf48a7281d77e5093af497987af91e8dd78ca2b2
Andrei Savich2 <Andrei_Savich2@epam.com>
2022-02-14 10:00:53 +0300

    Added field demo to field accesses in the nested static classes

--- JavaCore/src/main/java/com/epam/theory/nested/staticnested/Developer.java
+++ JavaCore/src/main/java/com/epam/theory/nested/staticnested/Developer.java
@@ -1,9 +1,23 @@
 package com.epam.theory.nested.staticnested;
 
+/*
+/Nested static classes have access to all static fields from
+outer class, however with "private" modifier
+Outer class doesn't have access to fields from nested class
+ */
 public class Developer {
     private String name;
     private Level level;
     private Team team;
+
+    private int privateNonStaticField;
+    private static int privateStaticField;
+
+    //private int someNonStaticField = innerNonStaticField; - doesn't have access to nested non-static field from class
+    private int someNonStaticField = new Team("some name").innerNonStaticField;
+
+    //static field has access to static field from nested class
+    private static int someStaticField = Team.innerStaticField;
 
     //static nested enum
     public static enum Level {
@@ -12,6 +26,13 @@ public class Developer {
 
     //Static nested class
     public static class Team {
+        //int innerNonStaticField = privateNonStaticField; - doesn't work with non-static fields
+
+        //Access to outer static field
+         int innerNonStaticField = privateStaticField;
+
+        private static int innerStaticField;
+
         private String name;
 
         public Team(String name) {
