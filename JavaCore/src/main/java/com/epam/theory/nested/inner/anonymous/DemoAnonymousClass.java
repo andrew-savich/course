bfc1b47435e284602a7067a1d7e56ccd8ad3d433
Andrei Savich2 <Andrei_Savich2@epam.com>
2022-02-14 12:01:53 +0300

    Added example to access to variables in the anonymous classes

--- JavaCore/src/main/java/com/epam/theory/nested/inner/anonymous/DemoAnonymousClass.java
+++ JavaCore/src/main/java/com/epam/theory/nested/inner/anonymous/DemoAnonymousClass.java
@@ -1,6 +1,8 @@
 package com.epam.theory.nested.inner.anonymous;
 
 public class DemoAnonymousClass {
+    private static String message = "to do one-time work like anonymous";
+
     public static void main(String[] args) {
         Worker firstContractWorker = new ContractWorker();
         Worker secondContractWorker = new ContractWorker();
@@ -12,13 +14,18 @@ public class DemoAnonymousClass {
         secondContractWorker.doWork();
         oneTimeWorker.doWork();
 
+
         //Case with anonymous class
         Worker anonymousOneTimeWorker = new Worker() {
+            int someField = 1;
+
             @Override
             public void doWork() {
-                System.out.println("to do one-time work like anonymous");
+                System.out.println(message); //access to outer class field
             }
         };
+
+        //System.out.println(someField); - no access to field from anonymous class
 
         anonymousOneTimeWorker.doWork();
     }
