285d85efe72489ec56bca5e05acb047522c6f285
Andrei Savich2 <Andrei_Savich2@epam.com>
2022-02-14 11:49:32 +0300

    Added demo for to access field in the local classes

--- JavaCore/src/main/java/com/epam/theory/nested/inner/local/DemoLocalClass.java
+++ JavaCore/src/main/java/com/epam/theory/nested/inner/local/DemoLocalClass.java
@@ -2,6 +2,7 @@ package com.epam.theory.nested.inner.local;
 
 public class DemoLocalClass {
     public static void main(String[] args) {
-        System.out.println(StringMethods.getDifferenceLength().compare("22", "4444"));
+
+        System.out.println(new StringMethods().getDifferenceLength().compare("22", "4444"));
     }
 }
--- JavaCore/src/main/java/com/epam/theory/nested/inner/local/StringMethods.java
+++ JavaCore/src/main/java/com/epam/theory/nested/inner/local/StringMethods.java
@@ -2,28 +2,38 @@ package com.epam.theory.nested.inner.local;
 
 import java.util.Comparator;
 
+
 public class StringMethods {
-    static int one;
-    int two;
+    private int outerClassField1;
+    private static int outerStaticField;
+
 
     public Comparator<String> getDifferenceLength() {
-        int three = 3;
-        //three = 4;
+        int methodField1 = 1; //effective final (final or doesn't change self value)
+
+        int methodField2 = methodField1;
+        methodField2 = 2;
+
 
         class StringLengthComparator implements Comparator<String> {
+            private int localField1 = outerClassField1; //access to non-static field
+            private int localField2 = outerStaticField; //access to static field
+            private int localField3 = methodField1;
+
+            //private int localField4 = methodField2; - doesn't work, bcse methodField2 has been changed self value
+
+            //private int localField4 = methodAfterLocalClassField; - has no access to local field, bcse field was defined after inner local class
 
             @Override
             public int compare(String firstString, String secondString) {
                 int result = firstString.length() - secondString.length();
-
-                one = 1;
-                two = 2;
-                int local = three;
-
                 return result < 0 ? result * -1 : result;
             }
         }
 
+        int methodAfterLocalClassField = 0;
+
         return new StringLengthComparator();
     }
+
 }
