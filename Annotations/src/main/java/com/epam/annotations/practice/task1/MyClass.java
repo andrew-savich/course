8969465c60c66882dbcd6d8cfeb01422cb62cd18
Andrew Savich <savichandrew@gmail.com>
2022-02-23 11:54:47 +0300

    Changed methods from defauld to public

--- Annotations/src/main/java/com/epam/annotations/practice/task1/MyClass.java
+++ Annotations/src/main/java/com/epam/annotations/practice/task1/MyClass.java
@@ -3,12 +3,12 @@ package com.epam.annotations.practice.task1;
 public class MyClass {
 
     @Greeting
-    void firstMethod() {
+    public void firstMethod() {
         System.out.println("The first method");
     }
 
     @Greeting(value = "Hello from the second method")
-    void secondMethod() {
+    public void secondMethod() {
         System.out.println("The second method");
     }
 }
