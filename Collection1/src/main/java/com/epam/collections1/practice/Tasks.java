4bc302618457e83a8842b305c1bf30a71b04d6ea
Andrew Savich <savichandrew@gmail.com>
2022-03-16 14:33:20 +0300

    Added some theory and practice

---
+++ Collection1/src/main/java/com/epam/collections1/practice/Tasks.java
@@ -0,0 +1,15 @@
+package com.epam.collections1.practice;
+
+import java.util.Arrays;
+
+public class Tasks {
+    public static void main(String[] args) {
+        //Convert array to String with toString and with Arrays.toString
+        int[] someArray1 = new int[]{1, 2, 3, 4, 5};
+
+        System.out.println("toString: " + someArray1.toString());
+        System.out.println("Arrays.toString: " + Arrays.toString(someArray1));
+
+
+    }
+}
