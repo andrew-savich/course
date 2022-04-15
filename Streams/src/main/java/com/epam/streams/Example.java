2704a87e73cca0b19cef858eaef61db6ea7d102f
Andrew Savich <savichandrew@gmail.com>
2022-04-15 17:46:58 +0300

    Added theory about IO and Serialization

---
+++ Streams/src/main/java/com/epam/streams/Example.java
@@ -0,0 +1,10 @@
+package com.epam.streams;
+
+import java.io.*;
+
+
+public class Example {
+    public static void main(String[] args) {
+        File file = new File("c:\\Program Files\\");
+    }
+}
