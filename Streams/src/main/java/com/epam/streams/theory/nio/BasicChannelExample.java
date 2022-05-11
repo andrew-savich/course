751e2afb5d9f1287b5fdd298c280fd9ba26f27b9
andrew-savich <savichandrew@gmail.com>
2022-05-11 18:02:02 +0200

    Edited comment about this class

--- Streams/src/main/java/com/epam/streams/theory/nio/BasicChannelExample.java
+++ Streams/src/main/java/com/epam/streams/theory/nio/BasicChannelExample.java
@@ -5,7 +5,6 @@ import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
-//Here is a basic example that uses a FileChannel to read some data into a Buffer:
 //Простой пример как использовать FileChannel для чтения данных в буфер
 public class BasicChannelExample {
     public static void main(String[] args) throws IOException {
